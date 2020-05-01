9ba628c0a27ddcb142b0d7bdf0cb26b0ede35c30
Spotika4 <kobtsev.sasha@yandex.ru>
2020-05-01 13:49:57 +0300

    commit

---
+++ assets/src/js/my.js
@@ -0,0 +1,22 @@
+$(function() {
+	var menu = $(".menu-sidebar");
+	var menuItem = menu.find(".nav-item");
+
+	menuItem.find(".nav-link-parent").click(function() {
+		var parentItem = $(this).parent();
+		var isOpen = parentItem.hasClass("open");
+
+		menuItem.each(function(i, element) {
+			if ($(this).hasClass("open")) {
+				$(this).removeClass("open");
+			}
+		});
+
+		if (isOpen) {
+			parentItem.removeClass("open");
+		}
+		else {
+			parentItem.addClass("open");
+		}
+	});
+});
